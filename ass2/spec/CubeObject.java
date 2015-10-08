0be4d63cac26de099e0ca9c4f860e3dd773d013d
Mark Johnson <mark.johnson.ai@gmail.com>
2015-10-09 10:15:33 +1100

    Added implementation of a mesh, and tested on the cube objects. Need to figure out a way to allow for texturing/colouring and wireframe interchange.

--- ass2/spec/CubeObject.java
+++ ass2/spec/CubeObject.java
@@ -3,14 +3,38 @@ package ass2.spec;
 import javax.media.opengl.GL2;
 
 public class CubeObject extends GameObject {
-
+	Mesh m;
 	public CubeObject(GameObject parent) {
 		super(parent);
 		// TODO Auto-generated constructor stub
+		m = new Mesh();
+		m.addVertex(new double[]{-0.5, 0, -0.5}); // 0,0,0
+		m.addVertex(new double[]{-0.5, 0, 0.5});  // 0,0,1
+		m.addVertex(new double[]{0.5 , 0, 0.5});  // 1,0,1
+		m.addVertex(new double[]{0.5 , 0, -0.5}); // 1,0,0
+		m.addVertex(new double[]{-0.5, 1, -0.5});
+		m.addVertex(new double[]{-0.5, 1, 0.5});
+		m.addVertex(new double[]{0.5 , 1, 0.5});
+		m.addVertex(new double[]{0.5 , 1, -0.5});
+		
+		m.addNormal(new double[]{ 1,  0,  0});
+		m.addNormal(new double[]{ 0,  1,  0});
+		m.addNormal(new double[]{ 0,  0,  1});
+		m.addNormal(new double[]{-1,  0,  0});
+		m.addNormal(new double[]{ 0, -1,  0});
+		m.addNormal(new double[]{ 0,  0, -1});
+		
+		m.addFace(new int[]{0,3,2,1}, 4); // +y
+		m.addFace(new int[]{4,7,6,5}, 1); // -y
+		m.addFace(new int[]{3,7,6,2}, 0); // +x
+		m.addFace(new int[]{0,4,5,1}, 3); // -x
+		m.addFace(new int[]{1,5,6,2}, 2); // +z
+		m.addFace(new int[]{0,4,7,3}, 5); // -z
 	}
 	
 	@Override
 	public void drawSelf(GL2 gl) {
+//		m.draw(gl);
 		gl.glBegin(GL2.GL_QUADS);
 		{	
 			gl.glColor4f(0, 1, 0, 1);
---
+++ ass2/spec/Mesh.java
@@ -0,0 +1,80 @@
+package ass2.spec;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.media.opengl.GL2;
+
+public class Mesh {
+	private ArrayList<double[]> vertices;
+	private ArrayList<double[]> normals;
+	private ArrayList<int[]> face_verts;
+	private ArrayList<Integer> face_norms;
+	
+	public Mesh() {
+		vertices = new ArrayList<double[]>();
+		normals  = new ArrayList<double[]>();
+		face_verts = new ArrayList<int[]>();
+		face_norms= new ArrayList<Integer>();
+	}
+	
+	public void setVertices(List<double[]> l) {
+		vertices = new ArrayList<double[]>(l);
+	}
+	
+	public void setNormals(List<double[]> l) {
+		normals = new ArrayList<double[]>(l);
+	}
+	
+	public void setFaceVerts(List<int[]> l) {
+		face_verts = new ArrayList<int[]>(l);
+	}
+	
+	public void setFaceNorms(List<Integer> l) {
+		face_norms = new ArrayList<Integer>(l);
+	}
+	
+	public void addVertex(double[] v) {
+		vertices.add(v);
+	}
+	
+	public void addNormal(double[] n) {
+		normals.add(n);
+	}
+	
+	public void addFace(int[] vert_indices, int norm_index) {
+		face_verts.add(vert_indices);
+		face_norms.add(norm_index);
+	}
+	
+	public void draw (GL2 gl) {
+		gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+		if (face_verts.size() != face_norms.size()) {
+			System.err.println("Number of faces does not match number of normals");
+			return;
+		}
+		// Draw each face as a separate polygon
+		for (int i = 0; i < face_verts.size(); i++) {
+			gl.glBegin(GL2.GL_POLYGON);
+			{
+				double nx, ny, nz;
+				nx = normals.get(face_norms.get(i))[0];
+				ny = normals.get(face_norms.get(i))[1];
+				nz = normals.get(face_norms.get(i))[2];
+				gl.glNormal3d(nx, ny, nz);
+				for (int v_index : face_verts.get(i)) {
+					double x, y, z;
+					x = vertices.get(v_index)[0];
+					y = vertices.get(v_index)[1];
+					z = vertices.get(v_index)[2];
+					gl.glVertex3d(x, y, z);
+	
+					System.out.printf("%d\n", v_index);
+				}
+				System.out.println();
+			} 
+			gl.glEnd();
+		}
+		gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
+	}
+}
